7594e99b15ddc254fd24578fc9c91290b5a56ca9
Ali Hossain Nayan <94155479+Nayan1234l@users.noreply.github.com>
2024-03-07 15:51:03 +0600

    navbar-features-updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";

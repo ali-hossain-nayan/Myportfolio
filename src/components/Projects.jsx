6ebd0435763010c17b94d2b4281777a19e4450a6
Nayan1234l <ali15-13943@diu.edu.bd>
2024-07-07 15:43:44 +0600

    updated-office-lunch-project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import projImg1 from "../assets/img/project-img1.jpg";
 import projImg2 from "../assets/img/project-img2.jpg";
 import projImg3 from "../assets/img/project-img3.avif";
 import projImg4 from '../assets/img/project-img4.webp';
+import projImg5 from '../assets/img/project-img5.webp';
 import colorSharp2 from "../assets/img/color-sharp2.png";
 
 const Projects = () => {
@@ -46,68 +47,77 @@ const Projects = () => {
       deploymentLink: "https://nike-beta-blond.vercel.app/",
       Technology: ["React + Tailwind CSS"],
     },
+    {
+      title: "Office Lunch Management System",
+      description:
+        "For the easy way to choose  office employee lunch .  ",
+      imgUrl: projImg5,
+      githubLink: "https://github.com/ali-hossain-nayan/Office-Lunch-Management",
+      deploymentLink: "https://office-lunch-management-theta.vercel.app/",
+      Technology: ["React + Express.js + Node.js"],
+    },
     // Add more projects with their respective GitHub and deployment links
   ];
 
   return (
     <section className="project" id="projects" style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
-    <Container fluid style={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <Row style={{ height: "100%", width: "100%" }}>
-        <Col style={{ height: "100%", display: "flex", flexDirection: "column" }}>
-          <h2>Projects</h2>
-          <p>
-           Here is my some projects
-          </p>
-          <Tab.Container id="projects-tabs" defaultActiveKey="first">
-            <Nav
-              variant="pills"
-              className="nav-pills mb-5 justify-content-center align-items-center"
-              id="pills-tab"
-            >
-              <Nav.Item>
-                <Nav.Link eventKey="first">Front-end</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Ui-Ux</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">Backend</Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Tab.Content>
-              <Tab.Pane eventKey="first">
-                <Row>
-                  {projects.map((project, index) => {
-                    return <ProjectCard key={index} {...project} />;
-                  })}
-                </Row>
-              </Tab.Pane>
-              <Tab.Pane eventKey="second">
-                <Row>
-                  {projects
-                    .filter(
-                      (project) =>
-                        project.Technology &&
-                        project.Technology.includes("Ui-Ux")
-                    )
-                    .map((project, index) => {
+      <Container fluid style={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Row style={{ height: "100%", width: "100%" }}>
+          <Col style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+            <h2>Projects</h2>
+            <p>
+              Here is my some projects
+            </p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Front-end</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Ui-Ux</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Backend</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
                       return <ProjectCard key={index} {...project} />;
                     })}
-                </Row>
-              </Tab.Pane>
-              {/* Add more Tab.Pane components for additional tabs */}
-            </Tab.Content>
-          </Tab.Container>
-        </Col>
-      </Row>
-    </Container>
-    <img
-      className="background-image-right"
-      src={colorSharp2}
-      alt="Color Sharp 2"
-      style={{ position: "absolute", bottom: 0, right: 0, width: "50%", height: "auto" }}
-    />
-  </section>
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <Row>
+                    {projects
+                      .filter(
+                        (project) =>
+                          project.Technology &&
+                          project.Technology.includes("Ui-Ux")
+                      )
+                      .map((project, index) => {
+                        return <ProjectCard key={index} {...project} />;
+                      })}
+                  </Row>
+                </Tab.Pane>
+                {/* Add more Tab.Pane components for additional tabs */}
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="Color Sharp 2"
+        style={{ position: "absolute", bottom: 0, right: 0, width: "50%", height: "auto" }}
+      />
+    </section>
   );
 };
 

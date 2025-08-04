624503b0864f0170255d45bb90deddf42b87e21f
Nayan1234l <ali15-13943@diu.edu.bd>
2025-08-05 01:45:54 +0600

    updated-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./app.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../index.css";
-import "../App.css"
 
 const About = () => {
   return (
